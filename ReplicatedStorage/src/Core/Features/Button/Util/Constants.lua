7b2625075b429ce7a3e80c710802690a0145c7d8
darkfirebtw <121009303+C0ms@users.noreply.github.com>
2026-07-20 21:16:19 -0700

    Updated ReplicatedStorage/src/Core/Features/Button/Util/Constants.lua

---
+++ ReplicatedStorage/src/Core/Features/Button/Util/Constants.lua
@@ -0,0 +1,19 @@
+-- @ScriptType: ModuleScript
+--!nonstrict
+-- CONSTANTS
+-- BUTTON
+
+return {
+	PRESS_OFFSET = Vector3.new(0, -5, 0),
+	BOTTOM_OFFSET = Vector3.new(0, -0.35, 0),
+	
+	MAX_SCALE = 1.15,
+	STAT_MAX_SCALE = 1.14,
+	
+	DISPLAY_OFFSET = 4.5,
+	DISPLAY_SMOOTHNESS = 0.075,
+	DISPLAY_TILT = math.rad(22.5),
+	
+	DEFAULT_FOV = 70,
+	CLICK_FOV = 71,
+}
